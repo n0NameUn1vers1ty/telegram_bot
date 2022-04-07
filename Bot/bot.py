8b2f94607fb372f976030f5d7b3be3b156e679a4
yan <romanets2003@bk.ru>
2022-04-07 23:12:34 +0300

    fixed bugs with dependency, ready for work

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,11 +1,13 @@
 import telebot
 
 import config
+print('---bot started working\n---to turn off [ctrl + c]')
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.message_handler(content_types=['text'])
+
 def lala(message):
     bot.send_message(message.chat.id, message.text)
 
--- setup.py
+++ setup.py
@@ -4,9 +4,4 @@ setup(
     name='telegram_bot',
     version='1.0',
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'telegram_bot-cli = telegram_bot.Bot.bot:bot',
-        ],
-    },
 )
