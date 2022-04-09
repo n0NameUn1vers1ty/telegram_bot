40b04070c99feef1f204c0a10b4c72cc4458bdf7
Vladimir Suchkov <mroarab@gmail.com>
2022-04-09 23:51:08 +0300

    Только добавил парсер

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,15 +1,32 @@
 import telebot
 
+import requests
+from bs4 import BeautifulSoup
 import config
 print('---bot started working\n---to turn off [ctrl + c]')
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
-@bot.message_handler(content_types=['text'])
 
-def lala(message):
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Im working")
+
+@bot.message_handler(commands=['iTnews'])
+
+
+def ItNew(message):
+    url = 'https://droider.ru/' # ссылка на сайт
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    links = soup.find_all('a',class_='micronews-slider__item micronews-slider__item_link')
+
+    
+    for a in links:
+         bot.send_message(message.chat.id, a['href'])
+
+    
 
 
 # RUN
